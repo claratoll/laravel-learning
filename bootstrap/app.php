73a5da6b6e7b7f8a904247d05a77688105a59839
claratoll <clara.toll@gmail.com>
2024-04-30 13:53:27 +0200

    schedule

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Mail\PostCountMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Foundation\Application;
 use App\Http\Middleware\IsAdminMiddleware;
+use Illuminate\Console\Scheduling\Schedule;
 use App\Http\Middleware\CanViewPostMiddleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,6 +18,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias(['can-view-post' => CanViewPostMiddleware::class]);
         $middleware->alias(['is-admin' => IsAdminMiddleware::class]);
+    })
+    ->withSchedule(function(Schedule $schedule){
+        $schedule->call(function(){
+            Mail::to('admin@example.com')->send(new PostCountMail());
+        })->everyMinute();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
